fea1a89112d61e078382827a3454c2989ed4cdfe
fletchrichman <frichman3@gmail.com>
2013-04-25 21:47:31 -0600

    edited txt files and h files

--- code/connect4.h
+++ code/connect4.h
@@ -1,9 +1,8 @@
-
-
+//Displays the board in its current state
 void display();
 
-
+//Checks to see if either player has won
 bool check(int a, int b);
 
-
+//Drops the piece in the correct place. 
 int drop(int b, char player);
